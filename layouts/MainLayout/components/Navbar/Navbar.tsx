37ed8ff65bc40364bd3afea350fd17cb4b331924
Nguyễn Đắc Chí <chind@hasaki.vn>
2022-06-19 20:01:25 +0700

    link to homepage

--- layouts/MainLayout/components/Navbar/Navbar.tsx
+++ layouts/MainLayout/components/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 const ToggleDarkMode = dynamic(() => import('./components/ToggleDarkMode'), { ssr: false });
 
 const Navbar = (): JSX.Element => {
   return (
     <div className="h-12 flex items-center justify-between p-3 w-screen bg-sky-200 dark:bg-gray-800">
-      <span className="text-slate-900 dark:text-slate-50">coliamai</span>
+      <Link href="/">
+        <a className="text-slate-900 dark:text-slate-50">coliamai</a>
+      </Link>
       <ToggleDarkMode />
     </div>
   );
