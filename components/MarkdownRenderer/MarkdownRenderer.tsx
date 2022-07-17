38fd1c49f43eea255282bdab73aaf24f7d4eb2df
Nguyen Dac Chi <dacchi6761@gmail.com>
2022-07-17 08:11:21 +0700

    use nextimg for img in md

--- components/MarkdownRenderer/MarkdownRenderer.tsx
+++ components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import Image from 'next/image';
 
 type MarkdownRendererProps = {
   content: string;
@@ -7,7 +8,16 @@ type MarkdownRendererProps = {
 
 const MarkdownRenderer = ({ content }: MarkdownRendererProps): JSX.Element => {
   return (
-    <ReactMarkdown remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>{content}</ReactMarkdown>
+    <ReactMarkdown
+      remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+      components={{
+        img: ({ className, src, alt }) => (
+          <Image className={className} src={src!} alt={alt} width={1000} height={450} />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
   );
 };
 
